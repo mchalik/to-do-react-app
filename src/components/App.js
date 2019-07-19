a07357848c824847c4b7fbb2cce15b0dacd45ce3
Max Chalik <m.chalik@melstudio.ru>
2019-07-19 20:20:59 +0300

    save materials

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
-
+import Button from '@material-ui/core/Button'
 import '../styles/App.css';
 
 class App extends Component {
     render() {
         return (
             <div>
-            <h1>My React App!</h1>
-        </div>
-    );
+                <h1>My React App!</h1>
+                <Button>
+                    Press me
+                </Button>
+            </div>
+        );
     }
 }
 
